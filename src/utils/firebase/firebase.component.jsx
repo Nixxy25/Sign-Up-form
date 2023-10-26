e6824389ec20618d5975a5da5f7d3f4ae73241ab
Oyinkansola <tellaoyinkansola09@gmail.com>
2023-10-27 01:47:02 +0200

    yeah

--- src/utils/firebase/firebase.component.jsx
+++ src/utils/firebase/firebase.component.jsx
@@ -26,7 +26,7 @@ const firebaseConfig = {
   };
 
   // Initialize Firebase
-  const firebaseApp = initializeApp(firebaseConfig);
+ initializeApp(firebaseConfig);
 
   const googleProvider = new GoogleAuthProvider ();
 
